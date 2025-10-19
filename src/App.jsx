d78fa043e10c8a8024cb9bcc25cecc0aa72fb33a
Sujal <poojarisujal03@gmail.com>
2025-10-19 16:02:50 +0530

    Implemented lights

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import  gsap  from 'gsap'
+import { ScrollTrigger  } from 'gsap/all'
 
-
+gsap.registerPlugin(ScrollTrigger)
 const App = () => {
   return (
     <>
      <Navbar />
      <Hero />
+     <ProductViewer />
     </>
   )
 }
---
+++ src/components/three/ModelSwitcher.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModelSwitcher = () => {
+  return (
+    <div>ModelSwitcher</div>
+  )
+}
+
+export default ModelSwitcher
